eff4740ff06f866b91919d17f5f32257cf562b96
NinjaPhenix <ninjaphenix@hotmail.co.uk>
2020-08-24 18:14:08 +0100

    Initial port to 1.16.2, modid renamed to ninjaphenix_betterplacement, config code is commented out.

--- src/main/java/com/tterrag/betterplacement/BetterPlacement.java
+++ src/main/java/com/tterrag/betterplacement/BetterPlacement.java
@@ -1,45 +1,39 @@
 package com.tterrag.betterplacement;
 
-import static com.tterrag.betterplacement.BetterPlacement.MOD_ID;
-import static com.tterrag.betterplacement.BetterPlacement.MOD_NAME;
-import static com.tterrag.betterplacement.BetterPlacement.VERSION;
 
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Field;
 
 import net.minecraft.client.Minecraft;
-import net.minecraft.util.EnumFacing;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.RayTraceResult.Type;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.common.Mod.EventHandler;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent.ClientTickEvent;
-import net.minecraftforge.fml.common.gameevent.TickEvent.Phase;
-import net.minecraftforge.fml.relauncher.ReflectionHelper;
-import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 
-@Mod(modid = MOD_ID, name = MOD_NAME, version = VERSION, clientSideOnly = true, acceptedMinecraftVersions = "[1.9.4, 1.13)", guiFactory = "com.tterrag.betterplacement.GuiFactory")
-@EventBusSubscriber(Side.CLIENT)
+@Mod(value = BetterPlacement.MOD_ID)
+@EventBusSubscriber(value = Dist.CLIENT, bus = EventBusSubscriber.Bus.FORGE)
 public class BetterPlacement {
 
-    public static final String MOD_ID = "betterplacement";
-    public static final String MOD_NAME = "Better Placement";
-    public static final String VERSION = "@VERSION@";
+    public static final String MOD_ID = "ninjaphenix_betterplacement";
 
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event) {
-        Configs.load(event.getSuggestedConfigurationFile());
-    }
+    //@EventHandler
+    //public void preInit(FMLPreInitializationEvent event) {
+    //    Configs.load(event.getSuggestedConfigurationFile());
+    //}
 
     private static BlockPos lastTargetPos;
-    private static EnumFacing lastTargetSide;
+    private static Direction lastTargetSide;
 
-    private static final Field _rightClickDelayTimer = ReflectionHelper.findField(Minecraft.class, "field_71467_ac", "rightClickDelayTimer");
+    // todo: replace with at
+    private static final Field _rightClickDelayTimer = ObfuscationReflectionHelper.findField(Minecraft.class, "field_71467_ac");
 
     private static final MethodHandle getDelayTimer, setDelayTimer;
     static {
@@ -51,21 +45,24 @@ public class BetterPlacement {
             throw new RuntimeException(e);
         }
     }
+    //
 
     @SubscribeEvent
-    public static void onClientTick(ClientTickEvent event) throws Throwable {
-        if (event.phase == Phase.START && (!Configs.creativeOnly || Minecraft.getMinecraft().player.isCreative())) {
-            int timer = (int) getDelayTimer.invoke(Minecraft.getMinecraft());
-            RayTraceResult hover = Minecraft.getMinecraft().objectMouseOver;
-            if (hover != null && hover.typeOfHit == Type.BLOCK) {
-                BlockPos pos = hover.getBlockPos();
+    public static void onClientTick(TickEvent.ClientTickEvent event) throws Throwable {
+        if (event.phase == TickEvent.Phase.START /*&& (!Configs.creativeOnly || Minecraft.getMinecraft().player.isCreative())*/) {
+            int timer = (int) getDelayTimer.invoke(Minecraft.getInstance());
+            RayTraceResult hover = Minecraft.getInstance().objectMouseOver;
+            if (hover != null && hover.getType() == Type.BLOCK) {
+                BlockRayTraceResult hit = (BlockRayTraceResult) hover;
+                Direction face = hit.getFace();
+                BlockPos pos = hit.getPos();
                 if (timer > 0 && !pos.equals(lastTargetPos) && (lastTargetPos == null || !pos.equals(lastTargetPos.offset(lastTargetSide)))) {
-                    setDelayTimer.invoke(Minecraft.getMinecraft(), 0);
-                } else if (Configs.forceNewLoc && timer == 0 && pos.equals(lastTargetPos) && hover.sideHit == lastTargetSide) {
-                    setDelayTimer.invoke(Minecraft.getMinecraft(), 4);
-                }
+                    setDelayTimer.invoke(Minecraft.getInstance(), 0);
+                } /*else if (Configs.forceNewLoc && timer == 0 && pos.equals(lastTargetPos) && hit.getFace() == lastTargetSide) {
+                    setDelayTimer.invoke(Minecraft.getInstance(), 4);
+                } */
                 lastTargetPos = pos.toImmutable();
-                lastTargetSide = hover.sideHit;
+                lastTargetSide = face;
             }
         }
     }
