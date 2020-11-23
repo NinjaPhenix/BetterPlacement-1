be3f7b3f595fc8167640ad8c9e4a96a00f012601
NinjaPhenix <NinjaPhenix@hotmail.co.uk>
2020-11-23 22:37:01 +0000

    Tweak behaviour to allow vertical pillar-ing.

--- src/main/java/ninjaphenix/preciseblockplacing/fabric/PreciseBlockPlacing.java
+++ src/main/java/ninjaphenix/preciseblockplacing/fabric/PreciseBlockPlacing.java
@@ -3,7 +3,6 @@ package ninjaphenix.preciseblockplacing.fabric;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.loader.api.FabricLoader;
-
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.options.KeyBinding;
 import net.minecraft.client.resource.language.I18n;
@@ -12,7 +11,7 @@ import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
-
+import net.minecraft.util.math.Vec3d;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.lwjgl.glfw.GLFW;
@@ -26,19 +25,23 @@ import java.util.Properties;
 public class PreciseBlockPlacing implements ClientModInitializer {
 
     public static final PreciseBlockPlacing INSTANCE = new PreciseBlockPlacing();
-    private PreciseBlockPlacing() {}
+
+    private PreciseBlockPlacing() {
+    }
 
     public static final String MOD_ID = "preciseblockplacing";
     private static final Logger LOGGER = LogManager.getLogger(MOD_ID);
     private BlockPos lastTargetPos;
+    private Vec3d lastPlayerPos;
     private Direction lastTargetSide;
     private boolean CREATIVE_ONLY;
     private boolean FORCE_NEW_LOCATION;
     private KeyBinding toggleForceKeyBinding;
 
-    public void onClientTick(MinecraftClient client)
-    {
-        if (client.world == null) { return; }
+    public void onClientTick(MinecraftClient client) {
+        if (client.world == null) {
+            return;
+        }
         if (toggleForceKeyBinding.wasPressed()) {
             FORCE_NEW_LOCATION = !FORCE_NEW_LOCATION;
             // TranslatableText doesn't honor § formatting => LiteralText(I18n.translate)
@@ -51,12 +54,23 @@ public class PreciseBlockPlacing implements ClientModInitializer {
                 BlockHitResult hit = (BlockHitResult) hover;
                 Direction side = hit.getSide();
                 BlockPos pos = hit.getBlockPos();
-                if (timer > 0 && !pos.equals(lastTargetPos) && (lastTargetPos == null || !pos.equals(lastTargetPos.offset(lastTargetSide)))) {
-                    client.itemUseCooldown = 0;
-                } else if (FORCE_NEW_LOCATION && timer == 0 && pos.equals(lastTargetPos) && side == lastTargetSide) {
-                    client.itemUseCooldown = 4;
+                Vec3d playerPos = client.player.getPos();
+                BlockPos playerBlockPos = client.player.getBlockPos();
+                if (timer > 0) {
+                    if (!pos.equals(lastTargetPos) && (lastTargetPos == null || !pos.equals(lastTargetPos.offset(lastTargetSide)))) {
+                        client.itemUseCooldown = 0;
+                    }
+                } else {
+                    if (side == Direction.UP && !playerPos.equals(lastPlayerPos) && playerBlockPos.getX() == pos.getX() && playerBlockPos.getZ() == pos.getZ()) {
+                        client.itemUseCooldown = 0;
+                    } else {
+                        if (FORCE_NEW_LOCATION && pos.equals(lastTargetPos) && side == lastTargetSide) {
+                            client.itemUseCooldown = 4;
+                        }
+                    }
                 }
                 lastTargetPos = pos.toImmutable();
+                lastPlayerPos = playerPos;
                 lastTargetSide = side;
             }
         }
@@ -66,7 +80,7 @@ public class PreciseBlockPlacing implements ClientModInitializer {
     public void onInitializeClient() {
         final Properties config = new Properties();
         Path configPath = FabricLoader.getInstance().getConfigDir().resolve(MOD_ID + ".properties");
-        if(Files.exists(configPath)) {
+        if (Files.exists(configPath)) {
             try {
                 config.load(Files.newInputStream(configPath, StandardOpenOption.READ));
             } catch (IOException e) {
@@ -78,15 +92,15 @@ public class PreciseBlockPlacing implements ClientModInitializer {
             try {
                 config.store(Files.newOutputStream(configPath, StandardOpenOption.CREATE),
                         "Precise Block Placing Config\n" +
-                        "creativeOnly - If true, the modifications will only apply in creative mode.\n" +
-                        "forceNewLoc - When true, a held right click will never place two blocks in a row, the player must move the cursor to a new location.");
+                                "creativeOnly - If true, the modifications will only apply in creative mode.\n" +
+                                "forceNewLoc - When true, a held right click will never place two blocks in a row, the player must move the cursor to a new location.");
             } catch (IOException e) {
                 LOGGER.warn("Failed to save default config.", e);
             }
         }
         CREATIVE_ONLY = Boolean.parseBoolean(config.getProperty("creativeOnly"));
         FORCE_NEW_LOCATION = Boolean.parseBoolean(config.getProperty("forceNewLoc"));
-        
+
         KeyBindingHelper.registerKeyBinding(toggleForceKeyBinding = new KeyBinding("key." + MOD_ID + ".togglenewloc", GLFW.GLFW_KEY_UNKNOWN, "category." + MOD_ID));
     }
 
