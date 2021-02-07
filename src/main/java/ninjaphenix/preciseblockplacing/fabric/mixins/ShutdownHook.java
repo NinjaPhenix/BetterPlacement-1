59e1a747a7357fe6d283a8af491dbdb0fbd989e4
NinjaPhenix <NinjaPhenix@hotmail.co.uk>
2021-02-07 23:49:13 +0000

    Fix mixin crash... My bad.

--- src/main/java/ninjaphenix/preciseblockplacing/fabric/mixins/ShutdownHook.java
+++ src/main/java/ninjaphenix/preciseblockplacing/fabric/mixins/ShutdownHook.java
@@ -5,6 +5,7 @@ import ninjaphenix.preciseblockplacing.fabric.PreciseBlockPlacing;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.io.IOException;
 
@@ -12,7 +13,7 @@ import java.io.IOException;
 public class ShutdownHook {
 
     @Inject(method = "Lnet/minecraft/client/Minecraft;run()V", at = @At("TAIL"))
-    private void normalShutdown() {
+    private void normalShutdown(CallbackInfo info) {
         try {
             PreciseBlockPlacing.INSTANCE.saveCurrentConfig();
         } catch (IOException e) {
@@ -21,7 +22,7 @@ public class ShutdownHook {
     }
 
     @Inject(method = "Lnet/minecraft/client/Minecraft;emergencySave()V", at = @At("TAIL"))
-    private void emergencyShutdown() {
+    private void emergencyShutdown(CallbackInfo info) {
         try {
             PreciseBlockPlacing.INSTANCE.saveCurrentConfig();
         } catch (IOException e) {
